1f3cb6f286ddf22121a9dade9c5632b6b97532fe
onoie <onoie3@gmail.com>
2018-02-04 22:35:49 +0900

    add main

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+import (
+	"github.com/TransAssist/goz"
+)
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("HelloWorld")
+	goz.Complete()
+}
